6d5cc2b41f2996875f0d07192c64e0580192b0ac
Eugene <miss0110@nhnent.com>
2018-12-02 20:34:37 +0900

    first commit

---
+++ src/main/java/com/nhnent/operations/metrics/Customer.java
@@ -0,0 +1,26 @@
+package com.nhnent.operations.metrics;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email, name;
+
+    public Customer(String e, String n) {
+        this.email = e;
+        this.name = n;
+    }
+}
---
+++ src/main/java/com/nhnent/operations/metrics/CustomerRestController.java
@@ -0,0 +1,61 @@
+package com.nhnent.operations.metrics;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerRestController {
+
+    private final CounterService counterService;
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    CustomerRestController(CustomerRepository repository, CounterService counterService) {
+        this.customerRepository = repository;
+        this.counterService = counterService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public ResponseEntity<?> get(@PathVariable Long id) {
+        return this.customerRepository.findById(id).map(customer -> {
+            String metricName = metricPrefix("customers.read.found");
+            this.counterService.increment(metricName); // <2>
+            return ResponseEntity.ok(customer);
+        }).orElseGet(() -> {
+            String metricName = metricPrefix("customers.read.not-found");
+            this.counterService.increment(metricName); // <3>
+            return ResponseEntity.class.cast(ResponseEntity.notFound().build());
+        });
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> add(@RequestBody Customer newCustomer) {
+        this.customerRepository.save(newCustomer);
+        ServletUriComponentsBuilder url = ServletUriComponentsBuilder
+                .fromCurrentRequest();
+        URI location = url.path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        this.customerRepository.delete(id);
+        return ResponseEntity.notFound().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> get() {
+        return ResponseEntity.ok(this.customerRepository.findAll());
+    }
+
+    protected String metricPrefix(String k) {
+        return k;
+    }
+}
